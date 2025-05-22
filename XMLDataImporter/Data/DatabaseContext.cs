1ced0582449b0adba882e3af442d446e608d8fc8
CodeR4nger <ignabianchi@gmail.com>
2025-05-22 20:46:39 -0300

    Agregado de documentación

--- XMLDataImporter/Data/DatabaseContext.cs
+++ XMLDataImporter/Data/DatabaseContext.cs
@@ -4,11 +4,14 @@ using XMLDataImporter.Utils;
 
 namespace XMLDataImporter.Data;
 
+/// <summary>
+/// Representa el contexto de la base de datos utilizado por Entity Framework Core
+/// </summary>
 public class DatabaseContext : DbContext
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
-        
+
     }
 }
 
@@ -20,13 +23,18 @@ public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContex
     public DatabaseContextFactory() : this(new EnvironmentHandler())
     {
     }
-
+    /// <summary>
+    /// Constructor que permite inyectar una implementación personalizada de <see cref="IEnvironmentHandler"/>.
+    /// </summary>
     public DatabaseContextFactory(IEnvironmentHandler envHandler)
     {
         _envHandler = envHandler;
         _envHandler.Load();
     }
-
+    /// <summary>
+    /// Crea una nueva instancia de <see cref="DatabaseContext"/> usando los parámetros de conexión definidos en variables de entorno.
+    /// </summary>
+    /// <returns>Una nueva instancia de <see cref="DatabaseContext"/>.</returns>
     public DatabaseContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
--- XMLDataImporter/Utils/EnvironmentHandler.cs
+++ XMLDataImporter/Utils/EnvironmentHandler.cs
@@ -6,11 +6,15 @@ public interface IEnvironmentHandler
     void Load();
     string Get(string key);
 }
-
+/// <summary>
+/// Implementación de <see cref="IEnvironmentHandler"/> que utiliza DotNetEnv para cargar variables de entorno.
+/// </summary>
 public class EnvironmentHandler : IEnvironmentHandler
 {
     private bool _isLoaded = false;
-
+    /// <summary>
+    /// Carga las variables de entorno desde un archivo .env. Este método es seguro para llamarse múltiples veces, pero solo realiza la carga una vez.
+    /// </summary>
     public void Load()
     {
         if (!_isLoaded)
@@ -19,7 +23,12 @@ public class EnvironmentHandler : IEnvironmentHandler
             _isLoaded = true;
         }
     }
-
+    /// <summary>
+    /// Obtiene el valor de una variable de entorno. Lanza una excepción si la variable no está definida o está vacía.
+    /// </summary>
+    /// <param name="key">Clave de la variable de entorno.</param>
+    /// <returns>El valor correspondiente a la clave especificada.</returns>
+    /// <exception cref="InvalidOperationException">Se lanza si la variable no está definida o está vacía.</exception>
     public string Get(string key)
     {
         var envValue = DotNetEnv.Env.GetString(key);
